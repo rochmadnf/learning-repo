c3279a7d6b1676477681a1c238f2ed566f492f29
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-03-16 22:33:41 +0800

    fix app.js code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,20 @@
 import "./bootstrap";
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
-import { resolverPageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "RD Labs";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => {
-        resolverPageComponent(
+    resolve: (name) =>
+        resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob("./Pages/**/*.vue", { eager: true })
-        );
-    },
+            import.meta.glob("./Pages/**/*.vue")
+        ),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+        return createApp({ render: () => h(App, props) })
             .use(plugin)
             .mount(el);
     },
