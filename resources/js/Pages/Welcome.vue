b9fc70b23655392a4677b88ef74d3657a1e5d9d4
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-03-16 17:36:24 +0800

    Link Inertia

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mx-auto mt-5">
+        <h1 class="text-2xl font-bold">
+            Home
+        </h1>
+
+        <nav class="ml-8">
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/users">Users</Link>
+                </li>
+                <li>
+                    <Link href="/settings">Settings</Link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+import { Link } from '@inertiajs/vue3';
+
+export default {
+    components: { Link }
+}
+</script>
