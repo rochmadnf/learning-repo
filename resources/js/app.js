eaae9c87f67128b8e3205b74c8c12232fb6a401a
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-03-16 21:54:26 +0800

    refactor code app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolverPageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+const appName =
+    window.document.getElementsByTagName("title")[0]?.innerText || "RD Labs";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => {
+        resolverPageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue", { eager: true })
+        );
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+});
