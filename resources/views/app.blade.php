01ffe81f1e3b4dfe4902abbe7b249bdfe62adde0
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-03-16 21:45:08 +0800

    config root template

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+    <title>Inertia Vue</title>
+
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
