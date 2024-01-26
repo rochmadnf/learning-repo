ce027022c52808686a6f4deea356681f58ff310d
rochmadnf <rochmadnf@gmail.com>
2024-01-26 11:03:21 +0800

    integrate with MySQL

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mysqli = require('mysql2');
+
+const dbPool = mysqli.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'express_mysql',
+    password: 'Local##123',
+    waitForConnections: true,
+});
 
 // routes group
 const routes = require("./routes");
@@ -16,6 +25,21 @@ app.use(express.json());
 // e:middleware
 
 app.use('/users', routes.users);
+
+app.use('/', (req, res) => {
+    dbPool.execute(`SELECT * FROM users`, (err, rows) => {
+        if(err){
+            res.json({
+                message: 'Connection failed',
+            });
+        }
+
+        res.json({
+            message: 'Connection success',
+            data: rows
+        });
+    })
+})
 
 app.listen(port, () => {
     console.log(`App listening at http://127.0.0.1:${port}`);
