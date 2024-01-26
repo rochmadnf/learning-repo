007fa66b3899df8d5d168307d2db66396a9a23a1
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2024-01-26 15:53:38 +0800

    store user to db

--- src/controller/users.js
+++ src/controller/users.js
@@ -15,12 +15,21 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-const createNewUser = (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: "CREATE new user successfully",
-    data: req.body,
-  });
+const createNewUser = async (req, res) => {
+  const { body } = req;
+  try {
+    await model.createNewUser(body);
+
+    res.json({
+      message: "CREATE new user successfully",
+      data: body,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      server_message: error,
+    });
+  }
 };
 
 const updateUser = (req, res) => {
--- src/models/users.js
+++ src/models/users.js
@@ -5,6 +5,13 @@ const getAllUsers = () => {
   return dbPool.execute(query);
 };
 
+const createNewUser = (values) => {
+  return dbPool.execute(
+    `INSERT INTO users (name, email, address) VALUES('${values.name}', '${values.email}', '${values.address}')`
+  );
+};
+
 module.exports = {
   getAllUsers,
+  createNewUser,
 };
