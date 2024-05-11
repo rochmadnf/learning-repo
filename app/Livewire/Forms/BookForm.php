f48d56e0162e9c433eabdc8a2587ecd4b5e3e43f
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2024-05-11 23:43:38 +0800

    eps 9

--- app/Livewire/Forms/BookForm.php
+++ app/Livewire/Forms/BookForm.php
@@ -7,11 +7,16 @@ use Livewire\Form;
 
 class BookForm extends Form
 {
+    #[Validate(rule: ['required', 'min:5'], message: ['required' => ':attribute is required'], as: 'Title of the book')]
     public string $title = '';
+
+    #[Validate(rule: ['required', 'min:5'], message: ['required' => ':attribute is required'], as: 'Author of the book')]
     public string $author = '';
 
     public function create()
     {
+        $this->validate();
+
         auth()->user()->books()->create([
             'title' => $this->title,
             'author' => $this->author,
--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,16 +1,22 @@
 <div class="space-y-2 ">
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900">
-            <form wire:submit.prevent='submit' class="flex items-center gap-x-3">
+            <form wire:submit.prevent='submit' class="flex items-start gap-x-3">
                 <div class="grow">
                     <label for="title" class="sr-only">Book title</label>
-                    <input type="text" wire:model='form.title' id="title" placeholder="Book title"
+                    <input type="text" wire:model.blur='form.title' id="title" placeholder="Book title"
                         class="w-full border border-slate-300 rounded-lg">
+                    @error('form.title')
+                        <div class="mt-1 text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="grow">
                     <label for="author" class="sr-only">Book author</label>
-                    <input type="text" wire:model='form.author' id="author" placeholder="Book author"
+                    <input type="text" wire:model.blur='form.author' id="author" placeholder="Book author"
                         class="w-full border border-slate-300 rounded-lg">
+                    @error('form.author')
+                        <div class="mt-1 text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
                 <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-lg font-medium">Submit</button>
             </form>
