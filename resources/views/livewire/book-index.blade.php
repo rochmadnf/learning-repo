87803285c38e76fe39c8f937237a25d8989b78cb
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2024-05-11 14:29:11 +0800

    eps 5

--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,11 +1,5 @@
 <div class="space-y-2">
     @foreach ($books as $book)
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">
-                <h2 class="text-lg font-medium leading-snug">{{ $book->title }}</h2>
-
-                <div>by {{ $book->author }}</div>
-            </div>
-        </div>
+        <livewire:book-item :book="$book" wire:key='$book->id' />
     @endforeach
 </div>
---
+++ resources/views/livewire/book-item.blade.php
@@ -0,0 +1,6 @@
+<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="p-6 text-gray-900">
+        <h2 class="text-lg font-medium leading-snug">{{ $book->title }}</h2>
+        <div>by {{ $book->author }}</div>
+    </div>
+</div>
