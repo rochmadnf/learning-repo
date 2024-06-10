3a205e4f5647e75f0fbe3fcacc8a35c181f037c2
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2024-06-10 13:56:23 +0800

    update layout

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,19 +1,17 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
-import { PropsWithChildren } from 'react';
+import ApplicationLogo from "@/components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
+import { PropsWithChildren } from "react";
 
-export default function Guest({ children }: PropsWithChildren) {
-    return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+export function GuestLayout({ children }: PropsWithChildren) {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="flex w-full max-w-md flex-col space-y-6 overflow-hidden rounded-lg bg-background p-6 shadow-md">
+        <Link href="/" className="mx-auto w-20">
+          <ApplicationLogo className="size-20 fill-current text-gray-500" />
+        </Link>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
-            </div>
-        </div>
-    );
+        {children}
+      </div>
+    </div>
+  );
 }
