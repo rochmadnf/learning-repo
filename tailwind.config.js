d260e6e0ad36149c1325ce915559dd7c1a6094ee
rochmadnf <rochmadnf@gmail.com>
2025-02-27 16:51:12 +0800

    wip model and migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,42 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import { fontFamily } from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.tsx',
-    ],
+  darkMode: ["class"],
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/**/*.blade.php",
+    "./resources/js/**/*.tsx",
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", ...fontFamily.sans],
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
+        primary: {
+          DEFAULT: "oklch(var(--primary))",
+        },
+        success: {
+            DEFAULT: "oklch(var(--success))",
+        },
+        desctructive: {
+          DEFAULT: "hsl(var(--desctructive))",
+          foreground: "hsl(var(--desctructive-foreground))",
+        },
+      },
     },
+  },
 
-    plugins: [forms],
+  plugins: [forms],
 };
