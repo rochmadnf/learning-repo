5cf3265462022330d0a1c8b61651bfffe0d6d79f
rochmadnf <rochmadnf@gmail.com>
2025-06-26 14:37:48 +0800

    update users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->index();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->boolean('active_status')->default(1);
+            $table->boolean('is_online')->default(0);
+            $table->dateTime('last_seen')->default(now());
             $table->rememberToken();
             $table->timestamps();
         });
